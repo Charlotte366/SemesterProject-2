af360f54147d88235e6e37d6936794bc64503fa0
Charlotte366 <jc.oren@hotmail.com>
2022-08-24 09:46:50 +0200

    Update api.js

--- js/settings/api.js
+++ js/settings/api.js
@@ -1,1 +1,3 @@
 export const baseUrl ="https://charlotte-developer1.herokuapp.com/api/"; 
+export const API_HOTELS ="https://charlotte-developer1.herokuapp.com/api/shoes";
+//export const TOKEN_PATH = "auth/local";
